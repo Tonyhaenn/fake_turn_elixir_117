9ce63d1859af30a17c443e960eb2602b3a393632
Tony Haenn <tonyhaenn@gmail.com>
2024-07-24 08:34:39 -0400

    Minimally reproducing example

---
+++ lib/fake_turn_elixir117.ex
@@ -0,0 +1,18 @@
+defmodule FakeTurnElixir117 do
+  @moduledoc """
+  Documentation for `FakeTurnElixir117`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FakeTurnElixir117.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fake_turn_elixir117_test.exs
@@ -0,0 +1,8 @@
+defmodule FakeTurnElixir117Test do
+  use ExUnit.Case
+  doctest FakeTurnElixir117
+
+  test "greets the world" do
+    assert FakeTurnElixir117.hello() == :world
+  end
+end
